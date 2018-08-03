b3c05d1483f090b0d7c938cf2cc98e5ba8dd7619
Eric Dobbertin <eric@dairystatedesigns.com>
2018-08-03 13:34:34 -0500

    refactor: remove unused function

--- imports/plugins/included/payments-example/lib/api/example.js
+++ imports/plugins/included/payments-example/lib/api/example.js
@@ -1,17 +1,6 @@
 import { Meteor } from "meteor/meteor";
-import { Packages } from "/lib/collections";
 
 export const Example = {
-  accountOptions() {
-    const { settings } = Packages.findOne({
-      name: "reaction-paymentmethod"
-    });
-    if (!settings.apiKey) {
-      throw new Meteor.Error("invalid-credentials", "Invalid Credentials");
-    }
-    return settings.apiKey;
-  },
-
   authorize(cardInfo, paymentInfo, callback) {
     Meteor.call("exampleSubmit", "authorize", cardInfo, paymentInfo, callback);
   }
