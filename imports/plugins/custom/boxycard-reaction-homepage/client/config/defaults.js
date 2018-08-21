44bb5e1e522d50b894fdbd1e90ef0e6207906ecf
Allison Reilly <acreilly3@gmail.com>
2018-08-20 21:13:31 -0300

    Set up footer component

--- imports/plugins/custom/boxycard-reaction-homepage/client/config/defaults.js
+++ imports/plugins/custom/boxycard-reaction-homepage/client/config/defaults.js
@@ -3,5 +3,5 @@ import { Session } from "meteor/session";
 Session.set("INDEX_OPTIONS", {
   template: "HomePage",
   layoutHeader: "MeteorNav",
-  layoutFooter: "BoxycardFooter"
+  layoutFooter: "Footer"
 });
