bf8606be658a0c795162be380328dee0a96699d9
Mike Murray <hellomikemurray@gmail.com>
2018-10-04 08:52:59 -0700

    feat: add admin navbar component

--- imports/plugins/core/ui-navbar/client/index.js
+++ imports/plugins/core/ui-navbar/client/index.js
@@ -1,3 +1,4 @@
 export { default as Brand } from "./components/brand";
 export { default as Navbar } from "./containers/navbar";
+export { default as NavbarAdmin } from "./containers/navbarAdmin";
 export { default as NavBarCheckout } from "./containers/navbarCheckout";
