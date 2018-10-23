791751156c5a8ae9a0642f6e9f4d8533422cbd85
Ross Hadden <rosshadden@gmail.com>
2018-10-23 17:53:41 -0400

    Let redirectRules take params to query by.

--- imports/plugins/core/core/server/no-meteor/resolvers/Query/redirectRules.js
+++ imports/plugins/core/core/server/no-meteor/resolvers/Query/redirectRules.js
@@ -5,9 +5,13 @@
  * @summary get list of redirect rules
  * @param {Object} _ - unused
  * @param {Object} params - an object of all arguments that were sent by the client
+ * @param {String} params.from - path to redirect from
+ * @param {String} params.to - path to redirect to
+ * @param {Number} params.status - HTTP status code of the redirect
+ * @param {Boolean} params.enabled - whether the rule is enabled
  * @param {Object} context - an object containing the per-request state
  * @return {Promise<Array<Object>>} array of RedirectRules
  */
 export default async function redirectRules(_, params, context) {
-  return context.collections.RedirectRules.find({}).toArray();
+  return context.collections.RedirectRules.find(params).toArray();
 }
