649dac18b2b298f8023797fedefecf1f65f52206
Ross Hadden <rosshadden@gmail.com>
2018-10-23 17:39:41 -0400

    Added RedirectRule collection.

---
+++ imports/collections/schemas/routing.js
@@ -0,0 +1,38 @@
+import SimpleSchema from "simpl-schema";
+import { registerSchema } from "@reactioncommerce/schemas";
+
+/**
+ * @name RedirectRule
+ * @memberof Schemas
+ * @type {SimpleSchema}
+ * @property {String} [_id]
+ * @property {String} from
+ * @property {String} to
+ * @property {Number} [status=301]
+ * @property {Boolean} [enabled=true]
+ */
+export const RedirectRule = new SimpleSchema({
+  _id: {
+    type: String,
+    optional: true,
+    label: "Redirect rule ID"
+  },
+  from: {
+    type: String,
+    unique: true,
+    index: 1,
+    label: "Redirect from"
+  },
+  to: {
+    type: String,
+    label: "Redirect to"
+  },
+  status: {
+    type: SimpleSchema.Integer,
+    optional: true,
+    defaultValue: 301,
+    label: "Status"
+  }
+});
+
+registerSchema("RedirectRule", RedirectRule);
