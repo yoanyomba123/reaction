cfa30364d7b5725c27cf9be8214515186f3c8f16
Ross Hadden <rosshadden@gmail.com>
2018-10-25 11:42:26 -0400

    Let redirectRules query accept more filters.

--- imports/plugins/core/routing/server/no-meteor/resolvers/Query/redirectRule.js
+++ imports/plugins/core/routing/server/no-meteor/resolvers/Query/redirectRule.js
@@ -5,7 +5,7 @@
  * @summary get a specific redirect rule
  * @param {Object} _ - unused
  * @param {Object} params - an object of all arguments that were sent by the client
- * @param {String} params._id - id of a redirect rule
+ * @param {String} params.id - id of a redirect rule
  * @param {Object} context - an object containing the per-request state
  * @return {Promise<Object>} RedirectRule
  */
--- imports/plugins/core/routing/server/no-meteor/resolvers/Query/redirectRules.js
+++ imports/plugins/core/routing/server/no-meteor/resolvers/Query/redirectRules.js
@@ -4,14 +4,23 @@
  * @memberof Routes/GraphQL
  * @summary get list of redirect rules
  * @param {Object} _ - unused
- * @param {Object} params - an object of all arguments that were sent by the client
- * @param {String} params.from - path to redirect from
- * @param {String} params.to - path to redirect to
- * @param {Number} params.status - HTTP status code of the redirect
- * @param {Boolean} params.enabled - whether the rule is enabled
+ * @param {Object} [params] - an object of all arguments that were sent by the client
+ * @param {String} [params.id] - rule id
+ * @param {String} [params.from] - path to redirect from
+ * @param {String} [params.to] - path to redirect to
+ * @param {String} [params.type] - type of redirect
+ * @param {Number} [params.status] - HTTP status code of the redirect
+ * @param {Boolean} [params.enabled] - whether the rule is enabled
  * @param {Object} context - an object containing the per-request state
  * @return {Promise<Array<Object>>} array of RedirectRules
  */
 export default async function redirectRules(_, params, context) {
-  return context.collections.RedirectRules.find(params).toArray();
+  return context.collections.RedirectRules.find({
+    _id: params.id,
+    from: params.from,
+    to: params.to,
+    type: params.type,
+    status: params.status,
+    enabled: params.enabled
+  }).toArray();
 }
