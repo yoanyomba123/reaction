ceaf340cd74b36e5334ccf157bfa5cd587135b6e
Mike Murray <hellomikemurray@gmail.com>
2018-11-01 11:01:02 -0700

    feat: add registry entries for routing admin

--- imports/plugins/core/routing/register.js
+++ imports/plugins/core/routing/register.js
@@ -10,5 +10,34 @@ Reaction.registerPackage({
   graphQL: {
     resolvers,
     schemas
-  }
+  },
+  registry: [{
+    label: "URL Redirects",
+    description: "URL Redirects",
+    icon: "fa fa-table",
+    name: "routing/settings",
+    provides: ["settings"],
+    workflow: "coreRoutingWorkflow",
+    template: "routingSettings",
+    meta: {
+      actionView: {
+        dashboardSize: "md"
+      }
+    }
+  }],
+  layout: [{
+    layout: "coreLayout",
+    workflow: "coreRoutingWorkflow",
+    theme: "default",
+    enabled: true,
+    structure: {
+      template: "routingSettings",
+      layoutHeader: "NavBar",
+      layoutFooter: "",
+      notFound: "notFound",
+      dashboardHeader: "dashboardHeader",
+      dashboardControls: "dashboardControls",
+      adminControlsFooter: "adminControlsFooter"
+    }
+  }]
 });
