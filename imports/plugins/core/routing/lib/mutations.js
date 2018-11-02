b91bef38bef2bb55695a2fcbed2b0544936c2368
Mike Murray <hellomikemurray@gmail.com>
2018-11-02 09:25:57 -0700

    feat: add routing mutations

---
+++ imports/plugins/core/routing/lib/mutations.js
@@ -0,0 +1,39 @@
+import gql from "graphql-tag";
+
+export const addRedirectRuleMutation = gql`
+  mutation addRedirectRuleMutation($input: AddRedirectRuleInput!) {
+    addRedirectRule(input: $input) {
+      redirectRule {
+        _id
+        enabled
+        from
+        status
+        to
+        type
+      }
+    }
+  }
+`;
+
+export const updateRedirectRuleMutation = gql`
+  mutation updateRedirectRuleMutation($input: UpdateRedirectRuleInput!) {
+    updateRedirectRule(input: $input) {
+      redirectRule {
+        _id
+        enabled
+        from
+        status
+        to
+        type
+      }
+    }
+  }
+`;
+
+export const removeRedirectRuleMutation = gql`
+  mutation removeRedirectRuleMutation($input: RemoveRedirectRuleInput!) {
+    updateRedirectRule(input: $input) {
+      wasRemoved
+    }
+  }
+`;
