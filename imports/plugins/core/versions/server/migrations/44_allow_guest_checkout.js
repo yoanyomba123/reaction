db63af7983c044d36475dd056c99f9d707107175
Eric Dobbertin <eric@dairystatedesigns.com>
2018-11-13 09:21:02 -0600

    chore: add migration for allow guest checkout setting

---
+++ imports/plugins/core/versions/server/migrations/44_allow_guest_checkout.js
@@ -0,0 +1,44 @@
+import { Migrations } from "meteor/percolate:migrations";
+import { Packages, Shops } from "/lib/collections";
+
+Migrations.add({
+  version: 44,
+  up() {
+    Packages.find({
+      "name": "core",
+      "settings.public.allowGuestCheckout": { $exists: true }
+    }).forEach((corePkg) => {
+      Shops.update({ _id: corePkg.shopId }, {
+        $set: {
+          isGuestCheckoutAllowed: !!(corePkg.settings && corePkg.settings.public && corePkg.settings.public.allowGuestCheckout)
+        }
+      }, { bypassCollection2: true });
+
+      Packages.update({ _id: corePkg._id }, {
+        $unset: {
+          "settings.public.allowGuestCheckout": ""
+        }
+      }, { bypassCollection2: true });
+    });
+  },
+  down() {
+    Packages.find({
+      "name": "core",
+      "settings.public.allowGuestCheckout": { $exists: false }
+    }).forEach((corePkg) => {
+      const shop = Shops.findOne({ _id: corePkg.shopId }, { fields: { isGuestCheckoutAllowed: 1 } });
+
+      Packages.update({ _id: corePkg._id }, {
+        $set: {
+          "settings.public.allowGuestCheckout": shop.isGuestCheckoutAllowed
+        }
+      }, { bypassCollection2: true });
+
+      Shops.update({ _id: corePkg.shopId }, {
+        $unset: {
+          isGuestCheckoutAllowed: ""
+        }
+      }, { bypassCollection2: true });
+    });
+  }
+});
