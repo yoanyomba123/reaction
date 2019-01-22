81be91ff8fe7a52707b9adcd7476ddf4a4b797a9
Seun Martins <seundotme@gmail.com>
2019-01-22 18:02:10 +0100

    Fix comments
    Signed-off-by: Seun Martins <seundotme@gmail.com>

--- imports/plugins/included/sitemap-generator/server/no-meteor/resolvers/Query/sitemap.js
+++ imports/plugins/included/sitemap-generator/server/no-meteor/resolvers/Query/sitemap.js
@@ -4,19 +4,20 @@ import url from "url";
  * @name sitemapQuery
  * @method
  * @param {Object} _ - unused
- * @param {Object} [params] - an object of all arguments that were sent by the client
- * @param {String} [params.handle] - Sitemap's handle, as set in Sitemaps collection
- * @param {String} [params.shopAbsoluteUrl] - absolute URL of the shop the sitemap belongs to. The URL is used to find the shop with the domain of the URL
+ * @param {Object} params - an object of all arguments that were sent by the client
+ * @param {String} params.handle - Sitemap's handle, as set in Sitemaps collection
+ * @param {String} params.shopAbsoluteUrl - absolute URL of the shop the sitemap belongs to. The URL is used to find the shop with the domain of the URL
  * @param {Object} context -  an object containing the per-request state
- * @returns {String} - XML, with placeholders replaced (BASE_URL, LAST_MOD), or "" if not found
+ * @returns {String} - Sitemap object containing XML with placeholders replaced (BASE_URL, LAST_MOD)
  */
 export default async function sitemapQuery(_, params, context) {
+  console.log("params", params);
   const { Sitemaps, Shops } = context.collections;
   const { handle, shopAbsoluteUrl } = params;
 
   const domain = url.parse(shopAbsoluteUrl).hostname;
 
-  // ensure the domain requested is a know shop domain
+  // ensure the domain requested is a known shop domain
   const { _id: shopId } = await Shops.findOne({ domains: domain }) || {};
 
   console.log("shopId", shopId);
